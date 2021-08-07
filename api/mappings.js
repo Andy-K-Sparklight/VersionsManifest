a6732fcdb55d4af424187883f11daae968c20fbf
Andy K Rarity Sparklight <Twilight_Princess_Sparkle@outlook.com>
2021-08-07 14:36:18 +0800

    Update mappings.js

--- api/mappings.js
+++ api/mappings.js
@@ -4,7 +4,7 @@ module.exports = (req, res) => {
       console.log(req.query);
       let pt = req.query.path;
       console.log(pt);
-        const r = await got.get("https://launcher.mojang.com/v1/objects/"+pt, {responseType:"text"});
+        const r = await got.get("https://launcher.mojang.com/v1/objects/"+pt);
         res.setHeader("Cache-Control", "max-age=0, s-maxage=86400, stale-while-revalidate");
         res.status(200).send(r.body);
     })()
