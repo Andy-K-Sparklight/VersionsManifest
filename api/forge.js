3d6b685c4182949e7a120cb83fed355a47267c46
Andy K Rarity Sparklight <Twilight_Princess_Sparkle@outlook.com>
2021-08-25 20:33:26 +0800

    Update forge.js

--- api/forge.js
+++ api/forge.js
@@ -3,6 +3,6 @@ module.exports = (req, res) => {
     (async ()=>{
         const r = await got.get("https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json", {responseType:"json"});
         res.setHeader("Cache-Control", "max-age=0, s-maxage=43200, stale-while-revalidate");
-        res.status(200).json(r.body);
+        res.status(200).json(r.body).end();
     })()
 }
