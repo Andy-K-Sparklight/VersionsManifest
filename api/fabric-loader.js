060bbc0ddb9bd0d36d7090fcb34fdec532909b45
Andy K Rarity Sparklight <Twilight_Princess_Sparkle@outlook.com>
2021-08-25 20:33:56 +0800

    Update fabric-loader.js

--- api/fabric-loader.js
+++ api/fabric-loader.js
@@ -3,6 +3,6 @@ module.exports = (req, res) => {
     (async ()=>{
         const r = await got.get("https://meta.fabricmc.net/v2/versions/loader", {responseType:"json"});
         res.setHeader("Cache-Control", "max-age=0, s-maxage=43200, stale-while-revalidate");
-        res.status(200).json(r.body);
+        res.status(200).json(r.body).end();
     })()
 }
