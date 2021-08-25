6a75ce98cc0e0ca523f5f3d273b5c2df35471a45
Andy K Rarity Sparklight <Twilight_Princess_Sparkle@outlook.com>
2021-08-25 20:34:31 +0800

    Update mojang.js

--- api/mojang.js
+++ api/mojang.js
@@ -3,6 +3,6 @@ module.exports = (req, res) => {
     (async ()=>{
         const r = await got.get("https://launchermeta.mojang.com/mc/game/version_manifest.json", {responseType:"json"});
         res.setHeader("Cache-Control", "max-age=0, s-maxage=86400, stale-while-revalidate");
-        res.status(200).json(r.body);
+        res.status(200).json(r.body).end();
     })()
 }
